1e5409c5debd067e994dfa4a78275e4922934d3d
sophiedick <sophiefdick@gmail.com>
2015-12-07 13:43:10 +0000

    best working version to date. Monday 1:45pm

--- js/index.js
+++ js/index.js
@@ -1,128 +1,130 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 //Have console all variables logged these all and they work!
 
-var List1 = $(".List1");
-//console.log(List1); 
+	var List1 = $(".List1");
+	//console.log(List1); 
 
-var List1Array = [];
-List1.each(function() {
-	List1Array.push($(this).text());
-	console.log(List1Array);
-});
+	var List1Array = [];
+	List1.each(function() {
+		List1Array.push($(this).text());
+		console.log(List1Array);
+	});
 
-var List2 = $(".list2");
-//console.log(List2); 
+	var List2 = $(".list2");
+	//console.log(List2); 
 
-var List2Array = [];
-List2.each(function() {
-	List2Array.push($(this).text());
-	console.log(List2Array);
-});
+	var List2Array = [];
+	List2.each(function() {
+		List2Array.push($(this).text());
+		console.log(List2Array);
+	});
 
-var List3 = $(".list3");
-//console.log(List3); 
+	var List3 = $(".list3");
+	//console.log(List3); 
 
-var List3Array = [];
-List3.each(function() {
-	List3Array.push($(this).text());
-	console.log(List3Array);
-});
+	var List3Array = [];
+	List3.each(function() {
+		List3Array.push($(this).text());
+		console.log(List3Array);
+	});
 
-var List4 = $(".list4");
+	var List4 = $(".list4");
 
-var List4Array = [];
-List4.each(function() {
-	List4Array.push($(this).text());
-	console.log(List4Array);
-});
+	var List4Array = [];
+	List4.each(function() {
+		List4Array.push($(this).text());
+		console.log(List4Array);
+	});
 
-//console.log(List1Array); This works it pushes all the text to an array to be compared later. 
+	//console.log(List1Array); This works it pushes all the text to an array to be compared later. 
 
-var boyNames = $(".boyNames");
-// console.log(boyNames);
+	var boyNames = $(".boyNames");
+	// console.log(boyNames);
 
-var girlNames = $(".girlNames");
-// console.log(girlNames);
+	var girlNames = $(".girlNames");
+	// console.log(girlNames);
 
-var countries = $(".Countries");
-// console.log(countries);
+	var countries = $(".Countries");
+	// console.log(countries);
 
-var gameContainer = $("#gameContainer");
-// console.log(gameContainer);
+	var gameContainer = $("#gameContainer");
+	// console.log(gameContainer);
 
-var messages = $("#messages");
+	var messages = $("#messages");
 
-var playerText = $("#playerText");
-var submitButton = $("#submitButton");
-var myNewTextElement = $("<input type='text' value=''>");
+	var playerText = $("#playerText");
+	var submitButton = $("#submitButton");
+	var myNewTextElement = $("<input type='text' value=''>");
 
-var playerWords = [];
-var wrongGuesses = [];
+	var playerWords = [];
+	var wrongGuesses = [];
 
-var clickCount = 0;
-var counter = 0;
+	var clickCount = 0;
+	var counter = 0;
+	var gameCount = 0;
+	var playerOneTurn = true;
 
-//Instructions needed here:
+	//Instructions needed here:
+
+	playerOne = prompt("Player one please enter your name?");
+	console.log(playerOne);
+	$("#player1Name").html(playerOne);
+
+	playerTwo = prompt("Player two please enter your name?");
+	console.log(playerTwo);
+	$("#player2Name").html(playerTwo);
 
 
-
-// playerOne = prompt("Player one please enter your name?");
-// console.log(playerOne);
-
-// playerTwo = prompt("Player two please enter your name?");
-// console.log(playerTwo);
-
-
-//This function shows Grocery List then hides it. 
-function showList1(){
-	List1.show();
-	setTimeout(function(){
-		List1.hide();
-		//function to pop up instructions - tell user to start entering guesses and hit submit
-	}, 10000); 
+	//This function shows Grocery List then hides it. 
+	function showList1(){
+		List1.show();
+		setTimeout(function(){
+			List1.hide();
+			//function to pop up instructions - tell user to start entering guesses and hit submit
+		}, 10000); 
 	}
 
 
-function showList2(){
-	List2.show();
-	setTimeout(function(){
-		List2.hide();
-		//function to pop up instructions - tell user to start entering guesses and hit submit
-	}, 10000); 
+	function showList2(){
+		List2.show();
+		setTimeout(function(){
+			List2.hide();
+			//function to pop up instructions - tell user to start entering guesses and hit submit
+		}, 10000); 
 	}
 
 
-function showList3(){
-	List3.show();
-	setTimeout(function(){
-		List3.hide();
-		//function to pop up instructions - tell user to start entering guesses and hit submit
-	}, 10000); 
+	function showList3(){
+		List3.show();
+		setTimeout(function(){
+			List3.hide();
+			//function to pop up instructions - tell user to start entering guesses and hit submit
+		}, 10000); 
 	}
 
-function showList4(){
-	List4.show();
-	setTimeout(function(){
-		List4.hide();
-		//function to pop up instructions - tell user to start entering guesses and hit submit
-	}, 10000); 
+	function showList4(){
+		List4.show();
+		setTimeout(function(){
+			List4.hide();
+			//function to pop up instructions - tell user to start entering guesses and hit submit
+		}, 10000); 
 	}
 
+	//ANIMATION FUNCTIONALITY
 
+	// function animation(){
+	// 	$(".listContainer").slide(left: "250px");
+	// }
 
-//ANIMATION FUNCTIONALITY
+	// animation();
+	// Randomly ask user for inputs
 
-// function animation(){
-// 	$(".listContainer").slide(left: "250px");
-// }
+	// var thisGameAnswerNumber = pickAList(); 	
+	// var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
 
-// animation();
-// Randomly ask user for inputs
-
-var thisGameAnswerNumber = pickAList(); 	
-var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
-
+	var thisGameAnswerNumber; 	
+	var thisGameAnswerArray;
 
 	function listGenerator(number) {
 	    if (number === 1) {
@@ -131,99 +133,143 @@ var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
 	        return List2Array;
 	    } else if (number === 3) {
 	        return List3Array;
-	    } else if (number === 4){
+	    } else if (number === 4) {
 	        return List4Array;
 	    } 
 	}
 
-	function pickAList(){
-	    var randomNumber = Math.random();
-	    if (randomNumber < 0.24) {
-	        return 1;
-	    } else if (randomNumber < 0.49) {	    	
-	        return 2;
-	    } else if (randomNumber < 0.75) {
-	        return 3;
-	    } else {
-	        return 4;
-	    } 
+	function pickAList() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.24) {
+        return 1;
+    } else if (randomNumber < 0.49) {	    	
+        return 2;
+    } else if (randomNumber < 0.75) {
+        return 3;
+    } else {
+        return 4;
+    } 
 	}
 
 	function displayQuestion(number) {
-	    if (number ===1) {
-	    	messages.html("Please enter as many items as you can remember from the Grocery List");
-	    } else if (number === 2) {
-	    	messages.html("Please enter as many boys names as you can remember");
-	    } else if (number === 3) {
-	    	messages.html("Please enter as many girls names as you can remember");
-	    } else if (number ===4){
-	    	messages.html("Please enter as many country names as you can remember");
-	    } 
+    if (number ===1) {
+    	messages.html("Please enter as many items as you can remember from the Grocery List");
+    } else if (number === 2) {
+    	messages.html("Please enter as many boys names as you can remember");
+    } else if (number === 3) {
+    	messages.html("Please enter as many girls names as you can remember");
+    } else if (number ===4){
+    	messages.html("Please enter as many country names as you can remember");
+    } 
 	}
 
-// var thisGameAnswerNumber = pickAList(); 	
-// var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
+	// var thisGameAnswerNumber = pickAList(); 	
+	// var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
+	submitButton.click(function() {
+		checkWins(thisGameAnswerNumber); 
+	});
 
-
-function checkWins(number){
-submitButton.click(function(){ // This function pushes all the players guesses to an array named player words 
-
-// COMPARE ONE ELEMENT AT A TIME WITH THE ARRAY: THIS WORKS could be tidied up but mostly working. 
-
-	// console.log(playerText.val());
-	console.log(thisGameAnswerArray);
-	var gameArray = listGenerator(number);
-	
-	if(($.inArray(playerText.val(), gameArray)) != -1) {
+	function checkWins(number) { 
+		// This function pushes all the players guesses to an array named player words 
+		// COMPARE ONE ELEMENT AT A TIME WITH THE ARRAY: THIS WORKS could be tidied up but mostly working. 
+		// console.log(playerText.val());
 		console.log(thisGameAnswerArray);
-	    console.log("Correct");
-	    playerWords.push(playerText.val());
-		counter ++;
-		clickCount++
-    	console.log(counter);
+		var gameArray = listGenerator(number);
+		
+		if (($.inArray(playerText.val(), gameArray)) != -1) {	
+			// console.log(thisGameAnswerArray);
+		  console.log("Correct");
+
+		  playerWords.push(playerText.val());
+			counter++;
+			// $("#player1Scoreboard").html(counter);
+			clickCount++;
+			console.log(clickCount);
+	    console.log(counter);
+			console.log(playerWords);
+
+			if (clickCount == List1Array.length) {	
+				messages.html("That is the end of your turn, your score is " + counter + "/7!");
+				gameCount++;
+				console.log(gameCount);
+
+				playerOneTurn = !playerOneTurn;
+
+	    	if (gameCount % 2 != 0) {
+					console.log("test");
+			    resetBoard();	
+			  }
+			}
+		} else {
+			message.html("Incorrect, please try again!");
+			wrongGuesses.push(playerText.val());
+			console.log(wrongGuesses);
+			clickCount++;
+			console.log(clickCount);
+
+			if (clickCount == List1Array.length) {
+			  messages.html("That is the end of your turn, your score is " + counter + "/7!");
+				//playerOneTurn = !playerOneTurn;
+    		if(gameCount % 2 != 0) {
+					console.log("test");
+		    	resetBoard();
+				}
+			}
+		}
+	};
+	
+	function resetBoard(){
+		messages.html(playerTwo + " you're up! Get memorising");
+		thisGameAnswerNumber = pickAList(); 
+		thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
+		console.log(thisGameAnswerNumber);
+		playerText.val("");
+		console.log("RESET");
+		$("#player1Scoreboard").html(counter);
+		counter = 0;
+		clickCount = 0;
+		// console.log(clickCount);
+		// console.log(counter);
+		playerWords = [];
 		console.log(playerWords);
-    	
-		if(clickCount == List1Array.length){
-			messages.html("That is the end of your turn, your score is " + counter + "/7!");
-		    	}
+		// if (playerOneTurn != playerOneTurn) {
+		showList1(); // This calls the showListFunction - lists and shown and hidden.  
+		setTimeout(showList2, 10000);
+		setTimeout(showList3, 20000);
+		setTimeout(showList4, 30000);
+		setTimeout(function(){
+		// 	console.log(thisGameAnswerNumber);
+			displayQuestion(thisGameAnswerNumber);
+			// checkWins(thisGameAnswerNumber);
+		}, 40000);
+		if (clickCount == List1Array.length){
+			console.log("test");
+		 // $("#player2Scoreboard").html(counter);
 		}
 		
-		else {console.log("Incorrect, please try again!");
-			  wrongGuesses.push(playerText.val());
-			  console.log(wrongGuesses);
-			  clickCount++;
-			  console.log(clickCount);
-
-		if(clickCount == List1Array.length){
-		  	messages.html("That is the end of your turn, your score is " + counter + "/7!");
-		      	}
-		}
-	});
-}
-
-
-function startGame(){
-	var thisGameAnswerNumber = pickAList(); 
-	var numberToPass = thisGameAnswerNumber;
-	var thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
-	console.log(thisGameAnswerNumber);
-	// 	
-
-	showList1(); // This calls the showListFunction - lists and shown and hidden.  
-	setTimeout(showList2, 10000);
-	setTimeout(showList3, 20000);
-	setTimeout(showList4, 30000);
-
-	setTimeout(function(){
-		console.log(thisGameAnswerNumber);
-		displayQuestion(thisGameAnswerNumber);
-		checkWins(thisGameAnswerNumber);
-	}, 40000);
+		//}	
 	
 }
 
-startGame();
+	function startGame(){
+		thisGameAnswerNumber = pickAList(); 
+		thisGameAnswerArray = listGenerator(thisGameAnswerNumber);
+		console.log(thisGameAnswerNumber);
+		
+		showList1(); // This calls the showListFunction - lists and shown and hidden.  
+		setTimeout(showList2, 10000);
+		setTimeout(showList3, 20000);
+		setTimeout(showList4, 30000);
 
+		setTimeout(function(){
+			console.log(thisGameAnswerNumber);
+			displayQuestion(thisGameAnswerNumber);
+			// checkWins(thisGameAnswerNumber);
+		}, 40000);
+		// $("#player1Scoreboard").html(counter);
+	}
+
+	startGame();
 });
 
 
